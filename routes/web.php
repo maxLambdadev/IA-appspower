b1395e1d9b929fa4d3e51f27dae633fdb9248122
maxLambdadev <berishvilitemoraz11@gmail.com>
2024-01-20 04:03:08 -0500

    small change

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 
-//appspower.com
-Route::domain(env('APPSPOWER'))->group(base_path('routes/appspower/home.php'));
-//panel.appspower.com
-Route::domain(env('PANEL_APPSPOWER'))->group(base_path('routes/appspower/panel.php'));
+//appspowers.com
+// Route::domain(env('APPSPOWER'))->group(base_path('routes/appspower/home.php'));
+//panel.appspowers.com
+// Route::domain(env('PANEL_APPSPOWER'))->group(base_path('routes/appspower/panel.php'));
+Route::domain(env('APPSPOWER'))->group(base_path('routes/appspower/panel.php'));
